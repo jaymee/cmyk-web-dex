fbd4bfa2d0a3c66faf864dbdc57c311912a97e97
Kitty <25450688+jaymee@users.noreply.github.com>
2020-06-09 03:48:28 +0800

    Updates

---
+++ precache-manifest.a4f6fd7b4fb6cea9952bfdf3dbf1924e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4903c5643214f9898870eb046fd80f2b",
+    "url": "/cmyk-web-dex/index.html"
+  },
+  {
+    "revision": "4cb4336bfb5c92344c3e",
+    "url": "/cmyk-web-dex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66b89df868dfcb9a7512",
+    "url": "/cmyk-web-dex/static/js/2.61c693c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cmyk-web-dex/static/js/2.61c693c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb4336bfb5c92344c3e",
+    "url": "/cmyk-web-dex/static/js/main.c15e9e6f.chunk.js"
+  },
+  {
+    "revision": "d480f29db63c3eb0fa96",
+    "url": "/cmyk-web-dex/static/js/runtime-main.ea921fd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cmyk-web-dex/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cmyk-web-dex/precache-manifest.a4f6fd7b4fb6cea9952bfdf3dbf1924e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cmyk-web-dex/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
